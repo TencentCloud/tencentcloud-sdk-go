29e5e8032e2b42cf8d93222b5f253ab60b9b2fdb
tencentcloudapi <tencentcloudapi@tencent.com>
2018-12-24 20:22:13 +0800

    release 3.0.43

---
+++ tencentcloud/tke/v20180525/client.go
@@ -0,0 +1,144 @@
+// Copyright (c) 2017-2018 THL A29 Limited, a Tencent company. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v20180525
+
+import (
+    "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+    tchttp "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/http"
+    "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
+)
+
+const APIVersion = "2018-05-25"
+
+type Client struct {
+    common.Client
+}
+
+// Deprecated
+func NewClientWithSecretId(secretId, secretKey, region string) (client *Client, err error) {
+    cpf := profile.NewClientProfile()
+    client = &Client{}
+    client.Init(region).WithSecretId(secretId, secretKey).WithProfile(cpf)
+    return
+}
+
+func NewClient(credential *common.Credential, region string, clientProfile *profile.ClientProfile) (client *Client, err error) {
+    client = &Client{}
+    client.Init(region).
+        WithCredential(credential).
+        WithProfile(clientProfile)
+    return
+}
+
+
+func NewAddExistedInstancesRequest() (request *AddExistedInstancesRequest) {
+    request = &AddExistedInstancesRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("tke", APIVersion, "AddExistedInstances")
+    return
+}
+
+func NewAddExistedInstancesResponse() (response *AddExistedInstancesResponse) {
+    response = &AddExistedInstancesResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 添加已经存在的实例到集群
+func (c *Client) AddExistedInstances(request *AddExistedInstancesRequest) (response *AddExistedInstancesResponse, err error) {
+    if request == nil {
+        request = NewAddExistedInstancesRequest()
+    }
+    response = NewAddExistedInstancesResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDeleteClusterInstancesRequest() (request *DeleteClusterInstancesRequest) {
+    request = &DeleteClusterInstancesRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("tke", APIVersion, "DeleteClusterInstances")
+    return
+}
+
+func NewDeleteClusterInstancesResponse() (response *DeleteClusterInstancesResponse) {
+    response = &DeleteClusterInstancesResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 删除集群中的实例
+func (c *Client) DeleteClusterInstances(request *DeleteClusterInstancesRequest) (response *DeleteClusterInstancesResponse, err error) {
+    if request == nil {
+        request = NewDeleteClusterInstancesRequest()
+    }
+    response = NewDeleteClusterInstancesResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDescribeClusterInstancesRequest() (request *DescribeClusterInstancesRequest) {
+    request = &DescribeClusterInstancesRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("tke", APIVersion, "DescribeClusterInstances")
+    return
+}
+
+func NewDescribeClusterInstancesResponse() (response *DescribeClusterInstancesResponse) {
+    response = &DescribeClusterInstancesResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+//  查询集群下节点实例信息 
+func (c *Client) DescribeClusterInstances(request *DescribeClusterInstancesRequest) (response *DescribeClusterInstancesResponse, err error) {
+    if request == nil {
+        request = NewDescribeClusterInstancesRequest()
+    }
+    response = NewDescribeClusterInstancesResponse()
+    err = c.Send(request, response)
+    return
+}
+
+func NewDescribeClustersRequest() (request *DescribeClustersRequest) {
+    request = &DescribeClustersRequest{
+        BaseRequest: &tchttp.BaseRequest{},
+    }
+    request.Init().WithApiInfo("tke", APIVersion, "DescribeClusters")
+    return
+}
+
+func NewDescribeClustersResponse() (response *DescribeClustersResponse) {
+    response = &DescribeClustersResponse{
+        BaseResponse: &tchttp.BaseResponse{},
+    }
+    return
+}
+
+// 查询集群列表
+func (c *Client) DescribeClusters(request *DescribeClustersRequest) (response *DescribeClustersResponse, err error) {
+    if request == nil {
+        request = NewDescribeClustersRequest()
+    }
+    response = NewDescribeClustersResponse()
+    err = c.Send(request, response)
+    return
+}
---
+++ tencentcloud/tke/v20180525/models.go
@@ -0,0 +1,317 @@
+// Copyright (c) 2017-2018 THL A29 Limited, a Tencent company. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v20180525
+
+import (
+    "encoding/json"
+
+    tchttp "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/http"
+)
+
+type AddExistedInstancesRequest struct {
+	*tchttp.BaseRequest
+
+	// 集群ID
+	ClusterId *string `json:"ClusterId" name:"ClusterId"`
+
+	// 实例列表
+	InstanceIds []*string `json:"InstanceIds" name:"InstanceIds" list`
+
+	// 实例额外需要设置参数信息
+	InstanceAdvancedSettings *InstanceAdvancedSettings `json:"InstanceAdvancedSettings" name:"InstanceAdvancedSettings"`
+
+	// 增强服务。通过该参数可以指定是否开启云安全、云监控等服务。若不指定该参数，则默认开启云监控、云安全服务。
+	EnhancedService *EnhancedService `json:"EnhancedService" name:"EnhancedService"`
+
+	// 节点登录信息（目前仅支持使用Password或者单个KeyIds）
+	LoginSettings *LoginSettings `json:"LoginSettings" name:"LoginSettings"`
+
+	// 实例所属安全组。该参数可以通过调用 DescribeSecurityGroups 的返回值中的sgId字段来获取。若不指定该参数，则绑定默认安全组。（目前仅支持设置单个sgId）
+	SecurityGroupIds []*string `json:"SecurityGroupIds" name:"SecurityGroupIds" list`
+}
+
+func (r *AddExistedInstancesRequest) ToJsonString() string {
+    b, _ := json.Marshal(r)
+    return string(b)
+}
+
+func (r *AddExistedInstancesRequest) FromJsonString(s string) error {
+    return json.Unmarshal([]byte(s), &r)
+}
+
+type AddExistedInstancesResponse struct {
+	*tchttp.BaseResponse
+	Response *struct {
+
+		// 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
+		RequestId *string `json:"RequestId" name:"RequestId"`
+	} `json:"Response"`
+}
+
+func (r *AddExistedInstancesResponse) ToJsonString() string {
+    b, _ := json.Marshal(r)
+    return string(b)
+}
+
+func (r *AddExistedInstancesResponse) FromJsonString(s string) error {
+    return json.Unmarshal([]byte(s), &r)
+}
+
+type Cluster struct {
+
+	// 集群ID
+	ClusterId *string `json:"ClusterId" name:"ClusterId"`
+
+	// 集群名称
+	ClusterName *string `json:"ClusterName" name:"ClusterName"`
+
+	// 集群描述
+	ClusterDescription []*string `json:"ClusterDescription" name:"ClusterDescription" list`
+
+	// 集群版本（默认值为1.10.5）
+	ClusterVersion *string `json:"ClusterVersion" name:"ClusterVersion"`
+
+	// 集群系统。centos7.2x86_64 或者 ubuntu16.04.1 LTSx86_64，默认取值为ubuntu16.04.1 LTSx86_64
+	ClusterOs *string `json:"ClusterOs" name:"ClusterOs"`
+
+	// 集群类型，托管集群：MANAGED_CLUSTER，独立集群：INDEPENDENT_CLUSTER。
+	ClusterType *string `json:"ClusterType" name:"ClusterType"`
+
+	// 集群网络相关参数
+	ClusterNetworkSettings *ClusterNetworkSettings `json:"ClusterNetworkSettings" name:"ClusterNetworkSettings"`
+}
+
+type ClusterNetworkSettings struct {
+
+	// 用于分配集群容器和服务 IP 的 CIDR，不得与 VPC CIDR 冲突，也不得与同 VPC 内其他集群 CIDR 冲突
+	ClusterCIDR *string `json:"ClusterCIDR" name:"ClusterCIDR"`
+
+	// 是否忽略 ClusterCIDR 冲突错误, 默认不忽略
+	IgnoreClusterCIDRConflict *bool `json:"IgnoreClusterCIDRConflict" name:"IgnoreClusterCIDRConflict"`
+
+	// 集群中每个Node上最大的Pod数量(默认为256)
+	MaxNodePodNum *uint64 `json:"MaxNodePodNum" name:"MaxNodePodNum"`
+
+	// 集群最大的service数量(默认为256)
+	MaxClusterServiceNum *uint64 `json:"MaxClusterServiceNum" name:"MaxClusterServiceNum"`
+
+	// 是否启用IPVS(默认不开启)
+	IPVS *bool `json:"IPVS" name:"IPVS"`
+
+	// 集群的VPCID（如果创建空集群，为必传值，否则自动设置为和集群的节点保持一致）
+	VpcId *string `json:"VpcId" name:"VpcId"`
+}
+
+type DeleteClusterInstancesRequest struct {
+	*tchttp.BaseRequest
+
+	// 集群ID
+	ClusterId *string `json:"ClusterId" name:"ClusterId"`
+
+	// 主机InstanceId列表
+	InstanceIds []*string `json:"InstanceIds" name:"InstanceIds" list`
+
+	// 集群实例删除时的策略：terminate（销毁实例，仅支持按量计费云主机实例） retain （仅移除，保留实例）
+	InstanceDeleteMode *string `json:"InstanceDeleteMode" name:"InstanceDeleteMode"`
+}
+
+func (r *DeleteClusterInstancesRequest) ToJsonString() string {
+    b, _ := json.Marshal(r)
+    return string(b)
+}
+
+func (r *DeleteClusterInstancesRequest) FromJsonString(s string) error {
+    return json.Unmarshal([]byte(s), &r)
+}
+
+type DeleteClusterInstancesResponse struct {
+	*tchttp.BaseResponse
+	Response *struct {
+
+		// 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
+		RequestId *string `json:"RequestId" name:"RequestId"`
+	} `json:"Response"`
+}
+
+func (r *DeleteClusterInstancesResponse) ToJsonString() string {
+    b, _ := json.Marshal(r)
+    return string(b)
+}
+
+func (r *DeleteClusterInstancesResponse) FromJsonString(s string) error {
+    return json.Unmarshal([]byte(s), &r)
+}
+
+type DescribeClusterInstancesRequest struct {
+	*tchttp.BaseRequest
+
+	// 集群ID
+	ClusterId *string `json:"ClusterId" name:"ClusterId"`
+
+	// 偏移量,默认0
+	Offset *int64 `json:"Offset" name:"Offset"`
+
+	// 最大输出条数，默认20
+	Limit *int64 `json:"Limit" name:"Limit"`
+
+	// 需要获取的节点实例Id列表(默认为空，表示拉取集群下所有节点实例)
+	InstanceIds *string `json:"InstanceIds" name:"InstanceIds"`
+}
+
+func (r *DescribeClusterInstancesRequest) ToJsonString() string {
+    b, _ := json.Marshal(r)
+    return string(b)
+}
+
+func (r *DescribeClusterInstancesRequest) FromJsonString(s string) error {
+    return json.Unmarshal([]byte(s), &r)
+}
+
+type DescribeClusterInstancesResponse struct {
+	*tchttp.BaseResponse
+	Response *struct {
+
+		// 集群中实例总数
+		TotalCount *uint64 `json:"TotalCount" name:"TotalCount"`
+
+		// 集群中实例列表
+		InstanceSet []*Instance `json:"InstanceSet" name:"InstanceSet" list`
+
+		// 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
+		RequestId *string `json:"RequestId" name:"RequestId"`
+	} `json:"Response"`
+}
+
+func (r *DescribeClusterInstancesResponse) ToJsonString() string {
+    b, _ := json.Marshal(r)
+    return string(b)
+}
+
+func (r *DescribeClusterInstancesResponse) FromJsonString(s string) error {
+    return json.Unmarshal([]byte(s), &r)
+}
+
+type DescribeClustersRequest struct {
+	*tchttp.BaseRequest
+
+	// 集群ID列表(为空时，
+	// 表示获取账号下所有集群)
+	ClusterIds []*string `json:"ClusterIds" name:"ClusterIds" list`
+
+	// 偏移量,默认0
+	Offset *int64 `json:"Offset" name:"Offset"`
+
+	// 最大输出条数，默认20
+	Limit *int64 `json:"Limit" name:"Limit"`
+}
+
+func (r *DescribeClustersRequest) ToJsonString() string {
+    b, _ := json.Marshal(r)
+    return string(b)
+}
+
+func (r *DescribeClustersRequest) FromJsonString(s string) error {
+    return json.Unmarshal([]byte(s), &r)
+}
+
+type DescribeClustersResponse struct {
+	*tchttp.BaseResponse
+	Response *struct {
+
+		// 集群总个数
+		TotalCount *int64 `json:"TotalCount" name:"TotalCount"`
+
+		// 集群信息列表
+		Clusters []*Cluster `json:"Clusters" name:"Clusters" list`
+
+		// 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
+		RequestId *string `json:"RequestId" name:"RequestId"`
+	} `json:"Response"`
+}
+
+func (r *DescribeClustersResponse) ToJsonString() string {
+    b, _ := json.Marshal(r)
+    return string(b)
+}
+
+func (r *DescribeClustersResponse) FromJsonString(s string) error {
+    return json.Unmarshal([]byte(s), &r)
+}
+
+type EnhancedService struct {
+
+	// 开启云安全服务。若不指定该参数，则默认开启云安全服务。
+	SecurityService *RunSecurityServiceEnabled `json:"SecurityService" name:"SecurityService"`
+
+	// 开启云监控服务。若不指定该参数，则默认开启云监控服务。
+	MonitorService *RunMonitorServiceEnabled `json:"MonitorService" name:"MonitorService"`
+}
+
+type Instance struct {
+
+	// 实例的附加信息
+	InstanceAdvanceSettings *InstanceAdvancedSettings `json:"InstanceAdvanceSettings" name:"InstanceAdvanceSettings"`
+
+	// 实例ID
+	InstanceId *string `json:"InstanceId" name:"InstanceId"`
+
+	// 节点角色, MASTER, WORKER, ETCD, MASTER_ETCD,ALL, 默认为WORKER
+	InstanceRole *string `json:"InstanceRole" name:"InstanceRole"`
+
+	// 实例异常(或者处于初始化中)的原因
+	FailedReason *string `json:"FailedReason" name:"FailedReason"`
+
+	// 实例的状态（running 运行中，initializing 初始化中，failed 异常）
+	InstanceState *string `json:"InstanceState" name:"InstanceState"`
+}
+
+type InstanceAdvancedSettings struct {
+
+	// 数据盘挂载点, 默认不挂载数据盘. 已格式化的 ext3，ext4，xfs 文件系统的数据盘将直接挂载，其他文件系统或未格式化的数据盘将自动格式化为ext4 并挂载，请注意备份数据! 无数据盘或有多块数据盘的云主机此设置不生效。
+	MountTarget *string `json:"MountTarget" name:"MountTarget"`
+
+	// dockerd --graph 指定值, 默认为 /var/lib/docker
+	DockerGraphPath *string `json:"DockerGraphPath" name:"DockerGraphPath"`
+
+	// base64 编码的用户脚本, 此脚本会在 k8s 组件运行后执行, 需要用户保证脚本的可重入及重试逻辑, 脚本及其生成的日志文件可在节点的 /data/ccs_userscript/ 路径查看, 如果要求节点需要在进行初始化完成后才可加入调度, 可配合 unschedulable 参数使用, 在 userScript 最后初始化完成后, 添加 kubectl uncordon nodename --kubeconfig=/root/.kube/config 命令使节点加入调度
+	UserScript *string `json:"UserScript" name:"UserScript"`
+
+	// 设置加入的节点是否参与调度，默认值为0，表示参与调度；非0表示不参与调度, 待节点初始化完成之后, 可执行kubectl uncordon nodename使node加入调度.
+	Unschedulable *int64 `json:"Unschedulable" name:"Unschedulable"`
+}
+
+type LoginSettings struct {
+
+	// 实例登录密码。不同操作系统类型密码复杂度限制不一样，具体如下：<br><li>Linux实例密码必须8到16位，至少包括两项[a-z，A-Z]、[0-9] 和 [( ) ` ~ ! @ # $ % ^ & * - + = | { } [ ] : ; ' , . ? / ]中的特殊符号。<br><li>Windows实例密码必须12到16位，至少包括三项[a-z]，[A-Z]，[0-9] 和 [( ) ` ~ ! @ # $ % ^ & * - + = { } [ ] : ; ' , . ? /]中的特殊符号。<br><br>若不指定该参数，则由系统随机生成密码，并通过站内信方式通知到用户。
+	Password *string `json:"Password" name:"Password"`
+
+	// 密钥ID列表。关联密钥后，就可以通过对应的私钥来访问实例；KeyId可通过接口DescribeKeyPairs获取，密钥与密码不能同时指定，同时Windows操作系统不支持指定密钥。当前仅支持购买的时候指定一个密钥。
+	KeyIds []*string `json:"KeyIds" name:"KeyIds" list`
+
+	// 保持镜像的原始设置。该参数与Password或KeyIds.N不能同时指定。只有使用自定义镜像、共享镜像或外部导入镜像创建实例时才能指定该参数为TRUE。取值范围：<br><li>TRUE：表示保持镜像的登录设置<br><li>FALSE：表示不保持镜像的登录设置<br><br>默认取值：FALSE。
+	KeepImageLogin *string `json:"KeepImageLogin" name:"KeepImageLogin"`
+}
+
+type RunMonitorServiceEnabled struct {
+
+	// 是否开启[云监控](/document/product/248)服务。取值范围：<br><li>TRUE：表示开启云监控服务<br><li>FALSE：表示不开启云监控服务<br><br>默认取值：TRUE。
+	Enabled *bool `json:"Enabled" name:"Enabled"`
+}
+
+type RunSecurityServiceEnabled struct {
+
+	// 是否开启[云安全](/document/product/296)服务。取值范围：<br><li>TRUE：表示开启云安全服务<br><li>FALSE：表示不开启云安全服务<br><br>默认取值：TRUE。
+	Enabled *bool `json:"Enabled" name:"Enabled"`
+}
