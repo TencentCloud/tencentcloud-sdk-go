583ab445dd0e48aee1f223ac969ea28e7983c4f4
ZhiQiang Fan <aji.zqfan@gmail.com>
2019-12-06 16:25:09 +0800

    fix go get issue

---
+++ doc.go
@@ -0,0 +1,5 @@
+// go get -u github.com/tencentcloud/tencentcloud-sdk-go
+// package github.com/tencentcloud/tencentcloud-sdk-go: no Go files in /Users/xxx/go/src/github.com/tencentcloud/tencentcloud-sdk-go
+// FIXME: this is a workaround for above go get issue, a fake file does nothing but claims we are a go project.
+// Eventually we need something useful here, such as real documentation, version and etc.
+package doc
