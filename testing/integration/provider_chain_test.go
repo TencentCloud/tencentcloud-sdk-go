76e997af366911b6201528ca1edb48388810201f
julinzhang <julinzhang@tencent.com>
2021-07-27 19:45:28 +0800

    update README and example

--- testing/integration/provider_chain_test.go
+++ testing/integration/provider_chain_test.go
@@ -6,8 +6,17 @@ import (
 )
 
 func TestProviderChain(t *testing.T) {
-	rc := common.DefaultProviderChain()
-	_, err := rc.GetCredential()
+	// custom provider chain
+	/*
+	provider1 := common.DefaultCvmRoleProvider()
+	provider2 := common.DefaultEnvProvider()
+	customProviders := []common.Provider{provider1, provider2}
+	pc := common.NewProviderChain(customProviders)
+	*/
+
+	// default provider
+	pc := common.DefaultProviderChain()
+	_, err := pc.GetCredential()
 	if err != nil {
 		t.Error(err)
 	}
