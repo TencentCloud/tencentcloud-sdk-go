400f10ce37da5891db32858379b4788e1e3b30ff
lleiyyang <39645792+lleiyyang@users.noreply.github.com>
2021-11-25 10:50:19 +0800

    支持lz4压缩上传日志 (#162)
    * 支持lz4压缩上传日志
    
    Co-authored-by: samlyang <samlyang@tencent.com>

--- examples/cls/send_octet_stream.go
+++ examples/cls/send_octet_stream.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/pierrec/lz4"
 	"os"
 	"time"
 
@@ -38,7 +39,8 @@ func GenPbBody() []byte {
 }
 
 func main() {
-	region := "xxxx"//需要根据客户的实际地域自行填写
+	compressType := "xxx" //压缩方式, 目前只支持lz4, 客户根据需要填写(空字符串意味不压缩)
+	region := "xxx"//需要根据客户的实际地域自行填写
 	topicId := "xxxxxx-xxxxxx-xxxxxx-xxxxxx"//这里需要使用客户实际的topicId，不能输入topicname
 	hashKey := ""//可选参数，具体参考官方文档：https://cloud.tencent.com/document/product/614/59470
 	credential := common.NewCredential(
@@ -55,9 +57,25 @@ func main() {
 	headers := map[string]string{
 		"X-CLS-TopicId": topicId,
 		"X-CLS-HashKey": hashKey,
+		"X-CLS-CompressType": compressType,
 	}
 
 	body := GenPbBody()
+	if compressType == "lz4" {
+		out := make([]byte, lz4.CompressBlockBound(len(body)) + 1)
+		var hashTable [1 << 16]int
+		n, err := lz4.CompressBlock(body, out, hashTable[:])
+		if err != nil {
+			fmt.Printf("compress failed. err:%v\n", err.Error())
+			return
+		}
+		// copy incompressible data as lz4 format
+		if n == 0 {
+			n, _ = copyIncompressible(body, out)
+		}
+		body = out[:n]
+	}
+
 	request := tchttp.NewCommonRequest("cls", "2020-10-16", "UploadLog")
 	request.SetOctetStreamParameters(headers, body)
 	//创建common response
@@ -70,4 +88,30 @@ func main() {
 		return
 	}
 	fmt.Println(string(response.GetBody()))
+}
+
+func copyIncompressible(src, dst []byte) (int, error) {
+	lLen, dn := len(src), len(dst)
+	di := 0
+	if lLen < 0xF {
+		dst[di] = byte(lLen << 4)
+	} else {
+		dst[di] = 0xF0
+		if di++; di == dn {
+			return di, nil
+		}
+		lLen -= 0xF
+		for ; lLen >= 0xFF; lLen -= 0xFF {
+			dst[di] = 0xFF
+			if di++; di == dn {
+				return di, nil
+			}
+		}
+		dst[di] = byte(lLen)
+	}
+	if di++; di+len(src) > dn {
+		return di, nil
+	}
+	di += copy(dst[di:], src)
+	return di, nil
 }
