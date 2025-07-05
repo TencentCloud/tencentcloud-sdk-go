a4f4482974da37ac175aad3595b8d20429ef95eb
tencentcloudapi <tencentcloudapi@tencent.com>
2025-07-05 23:36:58 +0800

    release v1.0.1200

--- tencentcloud/bmvpc/v20180625/models.go
+++ tencentcloud/bmvpc/v20180625/models.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018 THL A29 Limited, a Tencent company. All Rights Reserved.
+// Copyright (c) 2017-2025 Tencent. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
